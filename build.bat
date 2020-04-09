fbaf1fd38b91c0bd132a48cc444c707039db8d92
Sokhibjon Orzikulov <sakhib.orzklv@gmail.com>
2020-04-09 23:46:50 +0500

    Deleting all unnecessary files (┬┬﹏┬┬)

--- build.bat
+++ build.bat
@@ -1,9 +1,21 @@
 @echo off
+
+echo Checking if BuildTools directory exists
 IF NOT EXIST BuildTools (
+    echo There is no BuildTools, creating BuildTools directory
     mkdir BuildTools
 )
+
+echo Entering BuildTools directory
 cd BuildTools
-wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+
+echo Downloading latest BuildTools.jar from spigot server
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar --no-check-certificate
+
+echo Executing 
 java -jar BuildTools.jar --rev latest
+
+echo Moving the result server.jar to main directory
 MOVE BuildTools/spigot-%Input%.jar ./spigot.jar
+
 pause
